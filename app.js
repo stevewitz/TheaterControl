2594ad2cfb522f8d8ffed0e7dec9eabad3e131ec
stevewitz <switzel@wizcomputing.coom>
2020-07-13 15:22:54 -0700

    Webserver starts port 9111

--- app.js
+++ app.js
@@ -1,41 +1,31 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var ejs = require('ejs');
 
 var app = express();
-
+app.use(express.static('public')); // set up the public directory as web accessible
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function (req, res) {
+  res.render('main.ejs',{ });
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/curtain', function (req, res) {
+  res.render('curtain.ejs',{ });
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+var http = require('http');
+
+var webserver = app.listen(9111, function () {
+  console.log('Webserver listening at http://' + webserver.address().address + ':' + webserver.address().port);
+
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
