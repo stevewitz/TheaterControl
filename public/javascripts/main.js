40bd2edffb7be8f2f864d8753bb0977b326e93bf
stevewitz <switzel@wizcomputing.coom>
2020-07-14 19:47:28 -0700

    added hardware files from rulsey

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,7 @@
 // created 7/13/2020 steve
 
 //const ws = new WebSocket("ws://localhost:8082");
-var ws = new WebSocket('ws://' + window.location.hostname + ':' + window.location.port);
+const ws = new WebSocket('ws://' + window.location.hostname + ':' + window.location.port);
 
 ws.addEventListener("open",() => {
     console.log(" websocket connected")
