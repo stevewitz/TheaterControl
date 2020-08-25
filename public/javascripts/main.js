aee787c12fc5d7d4eb1a131189b673af1ae05f37
steve <stevewitz@gmail.com>
2020-08-25 16:09:38 -0700

    close curtain with movie off

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -354,6 +354,7 @@ function devicesTurnOff(){
     oppoButton("oppoPower") ;
     denonButton("denonPower");
     jvcButton("jvcPower") /////  disabled while testing
+    curtainButton("curtainClose");
 
 }
 
